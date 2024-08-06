4a8640871539dfeac2acfe03660b6cb2fb020fdf
annamiolleh <anka301493@gmail.com>
2024-08-06 12:54:24 +0300

    Section footer(adapt)

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuButtonOn = document.querySelector('.menu-button__on');
     const menuButtonOff = document.querySelector('.menu-button__off');
     const smallMenu = document.querySelector('.small-menu');
-    const headerMenu = document.querySelector('.header')
+    const headerMenu = document.querySelector('.header');
+    const menuItems = document.querySelectorAll('.small-menu__link');
+
 
     function toggleMenu(elem) {
         elem.preventDefault();
@@ -12,8 +14,18 @@ document.addEventListener('DOMContentLoaded', () => {
         menuButtonOn.classList.toggle('menu-button__on--active');
         menuButtonOff.classList.toggle('menu-button__off--active');
         headerMenu.classList.toggle('header--active');
+        console.log(smallMenu.classList);
     }
 
+    menuItems.forEach(function(item) {
+        item.addEventListener('click', function() {
+            document.body.classList.toggle('body--lock');
+            smallMenu.classList.toggle('small-menu--active');
+            menuButtonOn.classList.toggle('menu-button__on--active');
+            menuButtonOff.classList.toggle('menu-button__off--active');
+            headerMenu.classList.toggle('header--active');
+        });
+    });
 
     // menuButtonOff.addEventListener('click', (event) => {
     //     event.preventDefault();
